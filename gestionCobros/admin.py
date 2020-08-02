44320bcb3c0b3070d6bab918044d5a5600943437
carlosarteaga <carteaga338@gmail.com>
2020-08-02 17:23:24 -0600

    Ambiente Desarrollo: 02/08/2020

--- gestionCobros/admin.py
+++ gestionCobros/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from gestionCobros.models import Puestos, Departamentos, Usuarios, prueba
+from gestionCobros.models import Puestos, Departamentos, Usuarios, Empresas, Clientes, Contactos, Productos, Recordatorios, Codigos, Motivos, Gestiones, Promesas, Pagos, LogCobros
 #Clientes, Gestiones, Codigos,Motivos,Recordatorios
 
 # Register your models here.
@@ -31,4 +31,14 @@ admin.site.register(Recordatorios)
 admin.site.register(Puestos)
 admin.site.register(Departamentos)
 admin.site.register(Usuarios)
-admin.site.register(prueba)
+admin.site.register(Empresas)
+admin.site.register(Clientes)
+admin.site.register(Contactos)
+admin.site.register(Productos)
+admin.site.register(Recordatorios)
+admin.site.register(Codigos)
+admin.site.register(Motivos)
+admin.site.register(Gestiones)
+admin.site.register(Promesas)
+admin.site.register(Pagos)
+admin.site.register(LogCobros)
--- gestionCobros/models.py
+++ gestionCobros/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Modificación de modelos con relaciones realzada el 2 de agosto del 2020 a las 12:42 pm
+# Modificación de modelos con relaciones realzada 
+# el 2 de agosto del 2020 a las 5:22 pm
 
 # se crearán 14 tablas relacionadas conectándose a MYSQL
 # las tablas seran:
@@ -83,12 +84,239 @@ class Usuarios(models.Model):
         ordering = ['primer_nombre']
 
 
-class prueba(models.Model):
-    id_prueba = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=35)
+class Empresas(models.Model):
+    id_empresa = models.AutoField(primary_key=True)
+    nombre_empresa = models.CharField(max_length=120)
+    descripcion = models.CharField(max_length=450,blank=True)
+    telefono = models.CharField(max_length=50, blank=True)
+    nombre_contacto = models.CharField(max_length=80,blank=True)
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
 
     def __str__(self):
-        return self.nombre
-
+        return self.nombre_empresa 
+    
     class Meta:
-        verbose_name_plural = "PruebaHub"
+        verbose_name_plural = 'Empresas'
+        ordering = ['id_empresa']
+
+class Clientes(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    id_empresa = models.ForeignKey(Empresas,on_delete=models.PROTECT)
+    id_usuario = models.ForeignKey(Usuarios,on_delete=models.PROTECT)
+    nombre = models.CharField(max_length=150)
+    tipo_id = models.CharField('Tipo de Identificación',max_length=1,default='1',choices= 
+    [('1','Cédula de Identidad'),
+     ('2','RTN'), 
+     ('3','Otro')])
+    identidad = models.CharField(max_length=30,blank=True)
+    fch_nacimiento = models.CharField(max_length=30,blank=True)
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return self.nombre 
+    
+    class Meta:
+        verbose_name_plural = 'Clientes'
+        ordering = ['nombre']
+
+class Contactos(models.Model):
+    id_contacto = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes,on_delete=models.PROTECT)
+    tipo_contacto = models.CharField(max_length=35,default='Teléfono', 
+    choices=[
+        ('Teléfono Trabajo','Teléfono Trabajo'),
+        ('Teléfono Casa','Teléfono Casa'),
+        ('Celular','Celular'),
+        ('Dirección Casa','Dirección Casa'),
+        ('Dirección Trabajo','Dirección Trabajo'),
+        ('Correo','Correo')])
+    descripcion = models.TextField()
+    comentario = models.CharField(max_length=200)
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return 'El cliente %s tiene un(a) %s , y su descripción es: %s' % (self.id_cliente,self.tipo_contacto,self.descripcion)
+    
+    class Meta:
+        verbose_name_plural = 'Contactos Cliente'
+        ordering = ['tipo_contacto']
+
+class Productos(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes,on_delete=models.PROTECT)
+    descripcion = models.TextField()
+    numero_producto = models.CharField(max_length=50)
+    dias_mora = models.IntegerField()
+    saldo_total = models.IntegerField()
+    capital = models.IntegerField()
+    intereses = models.IntegerField()
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return 'El cliente %s , Con número de producto: %s tiene un saldo total de %s lempiras' % (self.id_cliente,self.numero_producto,self.saldo_total)
+    
+    class Meta:
+        verbose_name_plural = 'Productos Cliente'
+        ordering = ['descripcion']
+
+class Recordatorios(models.Model):
+    id_recordatorio = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes,on_delete=models.PROTECT)
+    fch_recordatorio = models.DateField()
+    hora_recordatorio = models.TimeField()
+    descripción = models.TextField()
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return 'El cliente %s Recordatorio para el %s a las %s' % (self.id_cliente, self.fch_recordatorio,self.hora_recordatorio)
+    
+    class Meta:
+        verbose_name_plural = 'Recordatorios Cliente'
+        ordering = ['id_cliente']
+
+class Codigos(models.Model):
+    id_codigo = models.AutoField(primary_key=True)
+    descripción = models.CharField(max_length=100)
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return self.descripción
+    
+    class Meta:
+        verbose_name_plural = 'Códigos'
+        ordering = ['id_codigo']
+
+class Motivos(models.Model):
+    id_motivo = models.AutoField(primary_key=True)
+    id_codigo = models.ForeignKey(Codigos,on_delete=models.PROTECT)
+    descripción = models.CharField(max_length=100)
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return self.descripción
+    
+    class Meta:
+        verbose_name_plural = 'Motivos'
+        ordering = ['id_motivo']
+
+class Gestiones(models.Model):
+    id_gestion = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes, on_delete=models.PROTECT)
+    id_usuario = models.ForeignKey(Usuarios,on_delete=models.PROTECT)
+    id_codigo = models.ForeignKey(Codigos,on_delete=models.PROTECT)
+    id_motivo = models.ForeignKey(Motivos,on_delete=models.PROTECT)
+    fch_gestion = models.DateTimeField(auto_now_add=True)
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    descripcion = models.TextField()
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+    
+    def __str__(self):
+        return 'El cliente %s tiene una gestión el: %s' % (self.id_cliente ,self.fch_gestion)
+    
+    class Meta:
+        verbose_name_plural = 'Gestiones'
+        ordering = ['fch_gestion']
+
+class Promesas(models.Model):
+    id_promesa = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes,on_delete=models.PROTECT)
+    id_usuario = models.ForeignKey(Usuarios,on_delete=models.PROTECT)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    valor = models.IntegerField()
+    descripcion = models.TextField()
+    estatus_promesa = models.CharField('Estatus de la Promesa',max_length=1,default='1',choices=
+    [('1','Pendiente'),
+     ('2','Cumplida'),
+     ('3','Incumplida')])
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    descripcion = models.TextField()
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return 'El cliente %s tiene una promesa para el: %s por un valor de %s lempiras' % (self.id_cliente,self.fecha,self.valor)
+          
+    class Meta:
+        verbose_name_plural = 'Promesas'
+        ordering = ['id_cliente']
+    
+class Pagos(models.Model):
+    id_pago = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes,on_delete=models.PROTECT)
+    id_usuario = models.ForeignKey(Usuarios,on_delete=models.PROTECT)
+    id_promesa = models.ForeignKey(Promesas,on_delete=models.PROTECT, null=True)
+    fecha = models.DateField()
+    valor = models.IntegerField()
+    descripcion = models.TextField()
+    fch_creacion = models.DateTimeField(auto_now_add=True)
+    descripcion = models.TextField()
+    usuario_creacion = models.IntegerField()
+    fch_modificacion = models.CharField(max_length=35, blank=True)
+    usuario_modificacion = models.IntegerField(blank=True,null=True)
+    estado = models.CharField(max_length=1, default='1',choices=[('1','Activo'),('2','Inactivo')])
+
+    def __str__(self):
+        return 'Cliente Número: %s, fecha pago: %s, por el valor de: %s Lempiras.' % (self.id_cliente,self.fecha,self.valor)
+    
+    class Meta:
+        verbose_name_plural = 'Pagos'
+        ordering = ['id_cliente']
+
+class LogCobros(models.Model):
+    id_log = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Clientes,on_delete=models.PROTECT)
+    nombre_tabla = models.CharField(max_length=35)
+    fecha = models.DateTimeField(auto_now_add=True)
+    id_registro = models.IntegerField(blank=True,null=True)
+    tipo_accion = models.CharField('Tipo de Acción',max_length=35,choices=
+    [('Insertar','Insertar'),('Modificar','Modificar'),('Seleccionar','Seleccionar'),('Borrar','Borrar')])
+    Dato_anterior = models.CharField(max_length=400,blank=True)
+    Dato_despues = models.CharField(max_length=400,blank=True)
+    campo_afectado = models.CharField(max_length=35,blank=True)
+
+    def __str__(self):
+        return 'Cliente Número: %s, tabla afectada: %s' %( self.id_cliente,self.nombre_tabla)
+    
+    class Meta:
+        verbose_name_plural = 'Bitácora App Cobros'
+        ordering = ['fecha']
+    
+
+
+
+
+
